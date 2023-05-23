931341c730113d2e00f9d3e16569f8e81fe0377f
Anna Russi <annarussi@gmail.com>
2023-05-23 15:05:00 +0100

    tasks exercise done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
